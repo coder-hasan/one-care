f06e071f7043b91a20bb4a2e808303889e52f746
Mahmud Hasan <60518699+coder-hasan@users.noreply.github.com>
2020-06-01 10:21:42 +0600

    minalized this project

---
+++ assets/js/script.js
@@ -0,0 +1,195 @@
+function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+/* PointerEvents
+ ========================================================*/
+;
+(function ($) {
+    if (isIE() && isIE() < 11) {
+        $('html').addClass('lt-ie11');
+        $(document).ready(function () {
+            PointerEventsPolyfill.initialize({});
+        });
+    }
+})(jQuery);
+
+/* Stick up menus
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+
+        $(document).ready(function () {
+            $('#stuck_container').TMStickUp({})
+        });
+    }
+})(jQuery);
+
+/* ToTop
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+
+        $(document).ready(function () {
+            $().UItoTop({
+                easingType: 'easeOutQuart',
+                containerClass: 'toTop fas fa-angle-up'
+            });
+        });
+    }
+})(jQuery);
+
+/* EqualHeights
+ ========================================================*/
+;
+(function ($) {
+    var o = $('[data-equal-group]');
+    if (o.length > 0) {
+    }
+})(jQuery);
+
+/* Copyright Year
+ ========================================================*/
+;
+(function ($) {
+    var currentYear = (new Date).getFullYear();
+    $(document).ready(function () {
+        $("#copyright-year").text((new Date).getFullYear());
+    });
+})(jQuery);
+
+/* Navbar
+ ========================================================*/
+;
+(function ($) {
+})(jQuery);
+
+/* Mailform
+ =============================================*/
+;(function ($) {
+    var o = $('.rd-mailform');
+    if (o.length > 0) {
+
+        $(document).ready(function () {
+            var o = $('.rd-mailform');
+
+            if (o.length) {
+                o.rdMailForm({
+                    validator: {
+                        'constraints': {
+                            '@LettersOnly': {
+                                message: 'Please use letters only!'
+                            },
+                            '@NumbersOnly': {
+                                message: 'Please use numbers only!'
+                            },
+                            '@NotEmpty': {
+                                message: 'Field should not be empty!'
+                            },
+                            '@Email': {
+                                message: 'Enter valid e-mail address!'
+                            },
+                            '@Phone': {
+                                message: 'Enter valid phone number!'
+                            },
+                            '@Date': {
+                                message: 'Use MM/DD/YYYY format!'
+                            },
+                            '@SelectRequired': {
+                                message: 'Please choose an option!'
+                            }
+                        }
+                    }
+                }, {
+                    'MF000': 'Sent',
+                    'MF001': 'Recipients are not set!',
+                    'MF002': 'Form will not work locally!',
+                    'MF003': 'Please, define email field in your form!',
+                    'MF004': 'Please, define type of your form!',
+                    'MF254': 'Something went wrong with PHPMailer!',
+                    'MF255': 'There was an error submitting the form!'
+                });
+            }
+        });
+    }
+})(jQuery);
+
+/* Orientation tablet fix
+ ========================================================*/
+$(function () {
+    // IPad/IPhone
+    var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+        ua = navigator.userAgent,
+
+        gestureStart = function () {
+            viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";
+        },
+
+        scaleFix = function () {
+            if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+                viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+                document.addEventListener("gesturestart", gestureStart, false);
+            }
+        };
+
+    scaleFix();
+    // Menu Android
+    if (window.orientation != undefined) {
+        var regM = /ipod|ipad|iphone/gi,
+            result = ua.match(regM);
+        if (!result) {
+            $('.sf-menus li').each(function () {
+                if ($(">ul", this)[0]) {
+                    $(">a", this).toggle(
+                        function () {
+                            return false;
+                        },
+                        function () {
+                            window.location.href = $(this).attr("href");
+                        }
+                    );
+                }
+            })
+        }
+    }
+});
+var ua = navigator.userAgent.toLocaleLowerCase(),
+    regV = /ipod|ipad|iphone/gi,
+    result = ua.match(regV),
+    userScale = "";
+if (!result) {
+    userScale = ",user-scalable=0"
+}
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0' + userScale + '">');
+
+
+/* Camera
+ ========================================================*/
+;
+(function ($) {
+    var o = $('#camera');
+    if (o.length > 0) {
+
+        $(document).ready(function () {
+            o.camera({
+                autoAdvance: true,
+                height: '50.828%',
+                minHeight: '150px',
+                pagination: false,
+                thumbnails: false,
+                playPause: false,
+                hover: false,
+                loader: 'none',
+                navigation: true,
+                navigationHover: true,
+                mobileNavHover: false,
+                fx: 'simpleFade'
+            })
+        });
+    }
+})(jQuery);
+
